5419b67b99d5c51c66d173b62b82a3821c2632d8
smit-darji <smit.softvan@gmail.com>
2025-10-28 18:40:43 +0530

    Update image to smitdarji/k8s:v1

--- deploy_full_argo.sh
+++ deploy_full_argo.sh
@@ -7,7 +7,7 @@ set -e
 # ============================================================
 
 # --- Configuration ---
-APP_NAME="demo-app"
+APP_NAME="k8s-app"
 APP_NAMESPACE="webapps"
 ARGOCD_NAMESPACE="argocd"
 IMAGE_NAME="smitdarji/k8s"
