e6d052191b6d7e6c2722f806d9c6e44a4c87c240
smit-darji <smit.softvan@gmail.com>
2025-10-28 18:14:00 +0530

    Update image to smitdarji/k8s:v3 and ensure service

--- deploy_full_argo.sh
+++ deploy_full_argo.sh
@@ -7,11 +7,11 @@ set -e
 # ============================================================
 
 # --- Configuration ---
-APP_NAME="login-page"
+APP_NAME="login-app"
 APP_NAMESPACE="webapps"
 ARGOCD_NAMESPACE="argocd"
 IMAGE_NAME="smitdarji/k8s"
-IMAGE_TAG="0.0.1"
+IMAGE_TAG="v3"
 DEPLOY_FILE="k8s/deployment.yaml"
 APP_PATH="k8s"
 GIT_REPO_URL="https://github.com/smit-darji/argocd_cicd.git"
